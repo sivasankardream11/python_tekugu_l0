754f267e18843dd0743e92304c758d95d5f56be6
sivasankardream11 <121345383+sivasankardream11@users.noreply.github.com>
2023-11-22 15:14:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Sample JavaScript code (replace with your functionality)
+document.addEventListener('DOMContentLoaded', function () {
+    // Add your JavaScript code here
+    console.log('JavaScript loaded!');
+});
